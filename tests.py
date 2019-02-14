7741825114a7719493d4de64cb0c57069a87985f
anandijain <anandj@uchicago.edu>
2019-02-14 05:20:39 -0600

    added option for header in the Test class

--- tests.py
+++ tests.py
@@ -2,9 +2,9 @@ import sippy_lines as sl
 
 
 class Test:
-    def __init__(self, file_name, nba, run):
+    def __init__(self, file_name, nba, header, run):
         self.fn = file_name
-        self.sip = sl.Sippy(self.fn, 1, nba)
+        self.sip = sl.Sippy(self.fn, header, nba)
         self.sip.step()
         self.game = self.sip.games[0]
         if run != 0:
