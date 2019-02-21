6cc3526c78e175d31f25660d8bc9b41bcef979b6
anandijain <anandj@uchicago.edu>
2019-02-21 15:01:56 -0600

    cant tell if it is working and not detecting because of lag/latency or it is skipping/repeating.

--- tests.py
+++ tests.py
@@ -2,11 +2,11 @@ import sippy_lines as sl
 
 
 class Test:
-    def __init__(self, file_name, nba, header, run):
+    def __init__(self, file_name, header, game_type, run):
         self.fn = file_name
-        self.league = nba
+        self.gt = game_type
         self.header = header
-        self.sip = sl.Sippy(self.fn, header, nba)
+        self.sip = sl.Sippy(self.fn, header, self.gt)
         self.sip.step()
         print('num_games: ' + str(len(self.sip.games)))
         if len(self.sip.games) > 0:
